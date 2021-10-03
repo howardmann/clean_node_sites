dff80f1d28afd090a4ee213a8ada86c2709d00c8
howardmann <howardmann27@gmail.com>
2021-10-03 17:49:57 +1100

    initial commit

---
+++ db/memory/sites.js
@@ -0,0 +1,243 @@
+const sites = [{
+    "id": 1,
+    "name": "Robina Town Centre",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 2,
+    "name": "Watergardens Town Centre",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 3,
+    "name": "Canberra Centre",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 4,
+    "name": "Westpoint Shopping Centre",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 5,
+    "name": "Castle Towers",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 6,
+    "name": "Hyperdome",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 7,
+    "name": "Bathurst City Central",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 8,
+    "name": "Big Top",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 9,
+    "name": "Kippa Ring",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 10,
+    "name": "Pittwater Place",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 11,
+    "name": "Grand Central",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 12,
+    "name": "Eastland",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 13,
+    "name": "Woodgrove",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 14,
+    "name": "Castle Mall",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 15,
+    "name": "APVC Holdings",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 16,
+    "name": "Forest Lake",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 17,
+    "name": "Hinkler Central",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 18,
+    "name": "1 Chandos St",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 19,
+    "name": "Nerang Mall",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 20,
+    "name": "Merrifield City",
+    "temp_group_name": "QIC"
+  },
+  {
+    "id": 21,
+    "name": "Wollongong Shopping Centre",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 22,
+    "name": "Charlestown Square",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 23,
+    "name": "Melbourne Central Retail",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 24,
+    "name": "Rouse Hill Town Centre",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 25,
+    "name": "Highpoint Shopping Centre",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 26,
+    "name": "Chirnside Park Shopping Centre",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 27,
+    "name": "Parkmore Shopping Centre",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 28,
+    "name": "Casuarina Square",
+    "temp_group_name": "GPT"
+  },
+  {
+    "id": 29,
+    "name": "55 King Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 30,
+    "name": "570 Bourke Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 31,
+    "name": "737 Bourke Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 32,
+    "name": "300 La Trobe Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 33,
+    "name": "990 La Trobe Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 34,
+    "name": "130 Lonsdale Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 35,
+    "name": "242 Exhibition Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 36,
+    "name": "150 Lonsdale Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 37,
+    "name": "1 Nicholson Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 38,
+    "name": "200 Queen Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 39,
+    "name": "11-33 Exhibition Street",
+    "temp_group_name": "Charter Hall, Charter Hall VIC"
+  },
+  {
+    "id": 40,
+    "name": "143 Turbot Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 41,
+    "name": "69 Ann Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 42,
+    "name": "900 Ann Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 43,
+    "name": "32 Turbot Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 44,
+    "name": "175 Eagle Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 45,
+    "name": "85 George Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 46,
+    "name": "275 George Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 47,
+    "name": "28 MacGregor Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  },
+  {
+    "id": 48,
+    "name": "61 Mary Street",
+    "temp_group_name": "Charter Hall, Charter Hall QLD"
+  }
+]
+
+module.exports = sites
