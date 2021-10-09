d41545af60940e35760377e1f6b9c7a9c8ba5f75
howardmann <howardmann27@gmail.com>
2021-10-09 14:28:37 +1100

    Add makeRanking model with tests

--- models/ranking/index.js
+++ models/ranking/index.js
@@ -1,34 +1,50 @@
 let {requiredParam} = require('../../helpers')
 
-let validate = ({ranking}) => {
-  requiredParam(ranking, 'ranking')
 
-  let validatePosition = (ranking) => {
-    let validPos = [1,2,3].includes(ranking)
-    if(!validPos) {
-      throw new Error('ranking must be either 1,2 or 3.')
-    }    
+let makeRanking = (rankingInfo) => {  
+  let validate = ({ranking, site_id, group_id, month_end}) => {
+    requiredParam(ranking, 'ranking')
+    requiredParam(site_id, 'site_id')
+    requiredParam(group_id, 'group_id')
+    requiredParam(month_end, 'month_end')
+
+    let validatePosition = (ranking) => {
+      let validPos = [1,2,3].includes(ranking)
+      if(!validPos) {
+        throw new Error('ranking must be either 1,2 or 3.')
+      }    
+    }
+
+    let validateMonthEnd = (month_end) => {
+      let reg = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+      if (!month_end.match(reg)) {
+        throw new Error('month_end not valid date format dd/mm/yyyy.')
+      }
+    }
+
+
+    validatePosition(ranking)
+    validateMonthEnd(month_end)
+    return ({ranking, site_id, group_id, month_end})
   }
 
-  validatePosition(ranking)
-  return ({ranking})
-}
+  let normalize = ({ranking, site_id, group_id, month_end}) => {
+    let rankings = [
+      {pos: 1, word: 'First'},
+      {pos: 2, word: 'Second'},
+      {pos: 3, word: 'Third'}
+    ]
 
-let normalize = ({ranking}) => {
-  let rankings = [
-    {pos: 1, word: 'First'},
-    {pos: 2, word: 'Second'},
-    {pos: 3, word: 'Third'}
-  ]
+    let word = rankings.filter(el => el.pos === ranking)[0].word
 
-  let word = rankings.filter(el => el.pos === ranking)[0].word
-
-  return {
-    ranking: word
+    return {
+      ranking: word,
+      site_id,
+      group_id,
+      month_end
+    }
   }
-}
 
-let makeRanking = function(rankingInfo) {
   const validRanking = validate(rankingInfo)
   return normalRanking = normalize(validRanking)
 }
--- models/ranking/index.spec.js
+++ models/ranking/index.spec.js
@@ -2,24 +2,35 @@ let chai = require('chai');
 let expect = chai.expect;
 let makeRanking = require('./index')
 
+let valid_ranking = {
+  site_id: 1,
+  group_id: 1,
+  ranking: 2,
+  month_end: '30/09/2021'
+}
+
 describe('makeRanking', () => {
-  describe.only('ranking', () => {
+  describe('ranking', () => {
     it('throws error if ranking missing', async () => {
       let errorMessage = 'ranking cannot be null or undefined.'
       expect(() => {
-        makeRanking({ranking: null})
+        let {ranking, ...validInfo} = valid_ranking
+        makeRanking({ranking: null, ...validInfo})
       }).to.throw(errorMessage)
     })
 
     it('throws error if ranking is not one of First Second or Third', async () => {
       let errorMessage = 'ranking must be either 1,2 or 3.'
       expect(() => {
-        makeRanking({ranking: 4})
+        let {ranking, ...validInfo} = valid_ranking
+        makeRanking({ranking: 4, ...validInfo})
       }).to.throw(errorMessage)
     })
 
     it('adds ranking', () => {
-      let input = makeRanking({ranking: 1}).ranking
+      let {ranking, ...validInfo} = valid_ranking
+      let newRank = makeRanking({ranking: 1, ...validInfo})
+      let input = newRank.ranking
       let actual = 'First'
       expect(input).to.eql(actual)
     })
@@ -28,30 +39,38 @@ describe('makeRanking', () => {
   describe('site_id', () => {
     it('throws error if site_id missing', async () => {
       let errorMessage = 'site_id cannot be null or undefined.'
+      
       expect(() => {
-        makeRanking({site_id: null})
+        let {site_id, ...validInfo} = valid_ranking
+        makeRanking({site_id: null, ...validInfo})
       }).to.throw(errorMessage)
     })
-    it('throws error if site_id does not exist', async () => {
-      let errorMessage = 'site_id not found.'
-      expect(() => {
-        makeRanking({site_id: 42424242})
-      }).to.throw(errorMessage)
+
+
+    it('adds valid site_id', () => {
+      let newRank = makeRanking(valid_ranking)
+      
+      let input = newRank.site_id
+      let actual = valid_ranking.site_id
+      expect(input).to.eql(actual)
     })
+
   })
 
   describe('group_id', () => {
     it('throws error if group_id missing', async () => {
       let errorMessage = 'group_id cannot be null or undefined.'
       expect(() => {
-        makeRanking({group_id: null})
+        let {group_id, ...validInfo} = valid_ranking
+        makeRanking({group_id: null, ...validInfo})
       }).to.throw(errorMessage)
     })
-    it('throws error if group_id does not exist', async () => {
-      let errorMessage = 'group_id not found.'
-      expect(() => {
-        makeRanking({group_id: 42424242})
-      }).to.throw(errorMessage)
+    
+    it('adds group_id', () => {
+      let newRank = makeRanking(valid_ranking)      
+      let input = newRank.group_id
+      let actual = valid_ranking.group_id
+      expect(input).to.eql(actual)
     })
   })
 
@@ -59,18 +78,17 @@ describe('makeRanking', () => {
     it('throws error if month_end missing', async () => {
       let errorMessage = 'month_end cannot be null or undefined.'
       expect(() => {
-        makeRanking({
-          month_end: null
-        })
+        let {month_end, ...validInfo} = valid_ranking
+        makeRanking({month_end: null, ...validInfo})
       }).to.throw(errorMessage)
     })
     it('throws error if month_end not a valid date', async () => {
-      let errorMessage = 'month_end not valid date format dd-mm-yyyy.'
+      let errorMessage = 'month_end not valid date format dd/mm/yyyy.'
       expect(() => {
-        makeRanking({
-          month_end: '2021-02-02'
-        })
+        let {month_end, ...validInfo} = valid_ranking
+        makeRanking({month_end: '2021-09-30', ...validInfo})
       }).to.throw(errorMessage)
+      
     })
   })
 
